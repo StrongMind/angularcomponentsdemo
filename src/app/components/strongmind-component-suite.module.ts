ebcac1dc69eaff1e39fb96127f43cd05d3d061e2
Orion Kindel <cakekindel@gmail.com>
2019-03-28 13:56:26 -0700

    Add Module

---
+++ src/app/components/strongmind-component-suite.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { NumberPickerComponent } from './number-picker/number-picker.component';
+
+@NgModule({
+    declarations: [NumberPickerComponent],
+    exports: [NumberPickerComponent],
+})
+export class StrongmindComponentSuiteModule { }
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,1 +1,2 @@
+export * from './components/strongmind-component-suite.module';
 export * from './components/number-picker/number-picker.component';
