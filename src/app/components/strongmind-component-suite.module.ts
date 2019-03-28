de329d69e821d3a807e251195cb3f80b215e4598
Orion Kindel <cakekindel@gmail.com>
2019-03-28 15:43:37 -0700

    Add common, forms modules to SMCompModule

--- src/app/components/strongmind-component-suite.module.ts
+++ src/app/components/strongmind-component-suite.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { NumberPickerComponent } from './number-picker/number-picker.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
+    imports: [CommonModule, FormsModule],
     declarations: [NumberPickerComponent],
     exports: [NumberPickerComponent],
 })
